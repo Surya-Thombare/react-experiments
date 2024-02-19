67f6e675eb6fce9d71dd07f359b541a74e7d1392
Suryakant Thombre <suryathombre000@gmail.com>
2024-02-19 22:57:22 +0530

    Current time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,11 +6,24 @@ import ExportToExcel from './components/exportExcel.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0)
+
+  const currentTime = () => {
+    setInterval(() => {
+      const date = new Date()
+      setTime(date.toLocaleTimeString())
+    }, 1000);
+  } 
+
+useEffect(() => {
+  currentTime();
+},[time])
 
   return (
     <>
       
       <ExportToExcel />
+      <p>{time}</p>
     </>
   )
 }
